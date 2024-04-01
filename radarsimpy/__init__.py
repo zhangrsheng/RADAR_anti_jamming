d494effd34c462c28c5c6688aee050ac83140639
zhangrsheng <zhang.rongsheng@hotmail.com>
2024-04-01 17:05:39 +0800

    1

---
+++ radarsimpy/__init__.py
@@ -0,0 +1,23 @@
+r"""
+A Python module for radar simulation
+
+----------
+RadarSimPy - A Radar Simulator Built with Python
+Copyright (C) 2018 - PRESENT  radarsimx.com
+E-mail: info@radarsimx.com
+Website: https://radarsimx.com
+
+ ____           _            ____  _          __  __
+|  _ \ __ _  __| | __ _ _ __/ ___|(_)_ __ ___ \ \/ /
+| |_) / _` |/ _` |/ _` | '__\___ \| | '_ ` _ \ \  /
+|  _ < (_| | (_| | (_| | |   ___) | | | | | | |/  \
+|_| \_\__,_|\__,_|\__,_|_|  |____/|_|_| |_| |_/_/\_\
+
+"""
+
+# radar
+from .radar import Radar
+from .radar import Transmitter
+from .radar import Receiver
+
+__version__ = "12.0.0"
